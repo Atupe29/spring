ca48a25893a02e403c13e69a1cf0f84898e7e604
Arjun <atupe439@gmail.com>
2023-07-11 21:06:31 +0530

    updated

---
+++ src/test/java/com/jdc04/SpringfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
